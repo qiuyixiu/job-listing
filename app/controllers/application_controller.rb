f36a5682a94d1f739aa6d5091b979920b7d935ce
imac <imac@imacdeiMac.local>
2017-04-18 21:24:21 +0800

    implement game first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def require_is_admin
     unless current_user.admin?
-      flash[:alert] = 'You are not admin'
+        flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
   end
