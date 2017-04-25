e5967fb26bc8a5f347c79a1f46008dc979bd183e
yixiu <aken@matoMac-mini-3.local>
2017-04-25 17:48:20 +0800

    implement category1

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,7 +2,7 @@ class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create, :update, :edit, :destroy, :index]
   before_action :require_is_admin
   layout "admin"
-  
+
   def index
     @jobs = Job.all
   end
@@ -62,7 +62,7 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :city)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :city, :category)
   end
 
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,15 +3,21 @@ class JobsController < ApplicationController
   before_action :validate_search_key, only: [:search]
 
   def index
+    @jobs = Job.published
+
+    if params[:category].present?
+      @category = params[:category]
+      @jobs = @jobs.where(:category => @category)
+    end
     @jobs = case params[:order]
             when 'by_lower_bound'
-              Job.where(is_hidden: false).order('wage_lower_bound DESC')
+              Job.published.order('wage_lower_bound DESC')
             when 'by_upper_bound'
-              Job.where(is_hidden: false).order('wage_upper_bound DESC')
+              Job.published.order('wage_upper_bound DESC')
             else
-              Job.where(is_hidden: false).order('created_at DESC')
+              Job.published.recent.paginate(:page => params[:page], :per_page => 5)
             end
-    @jobs = @jobs.paginate(:page => params[:page], :per_page => 5)
+
   end
 
   def new
@@ -84,7 +90,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :city)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :city, :category)
   end
 
 end
